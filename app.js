b9dba183883ac034b7149975205091fb8f95448f
k7sme <k7s.me@outlook.com>
2025-08-07 23:38:28 -0500

    fb whatsapp initial commit

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
+// Import Express.js
 const express = require('express');
-const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
 
+//load configuration from .env file
+dotenv.config();
+
+// Create an Express app
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+// Route for GET requests
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my Express app running on AWS EC2.');
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
 });
 
+// Route for POST requests
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
+// Health check endpoint
+app.get('/api/health', (req, res) => {
+  console.log('Health check endpoint hit');
+  res.json({
+    status: 'healthy-succesful',
+    timestamp: new Date().toISOString(),
+    version: process.env.npm_package_version
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Unhandled error:', err);
+  res.status(500).json({ success: false, error: 'Internal server error' });
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`\nListening on port ${port}\n`);
 });
